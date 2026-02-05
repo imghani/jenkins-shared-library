a2936443f207e9d89572db94f4aace9ef1340b2d
FaradayWicks <uzairhussan01@gmail.com>
2026-02-05 17:26:13 +0500

    remove the --build

--- vars/deployContainer.groovy
+++ vars/deployContainer.groovy
@@ -1,4 +1,4 @@
 def call(String serviceName, String deployLogDir) {
     sh "docker-compose pull ${serviceName} >> ${deployLogDir}/deploy_\$(date +%F_%T).log 2>&1"
-    sh "docker-compose up -d --no-deps --build ${serviceName}"
+    sh "docker-compose up -d --no-deps ${serviceName}"
 }
